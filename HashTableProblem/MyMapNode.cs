e4981e6162e60dd107f1208014ff54a9864df020
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-20 20:46:04 +0530

    HashTable Implementation

---
+++ HashTableProblem/MyMapNode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableProblem
+{
+    public class MyMapNode<K, V>
+    {
+        LinkedList<KeyValue<K, V>> list = new LinkedList<KeyValue<K, V>>();
+        public void Add (K key, V value)
+        {
+            KeyValue<K, V> obj = new KeyValue<K, V>() { key = key, value = value};
+            list.AddLast(obj);
+        }
+        public void GetFreq()
+        {
+            int count = 0;
+            if(list.Count == 0)
+            {
+                Console.WriteLine("HashTable is empty");
+                return;
+            }
+            foreach(KeyValue<K, V> obj in list)
+            {
+               foreach(KeyValue<K, V> pair in list) 
+               {
+                    if (obj.value.Equals(pair.value))
+                        count++;
+               }
+                Console.WriteLine("Count of "+ obj.value +" is " + count);
+                count = 0;
+            }
+        }
+    }
+    public struct KeyValue<K, V>
+    {
+        public K key;
+        public V value; 
+    }
+}
---
+++ HashTableProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HashTableProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMapNode<int, string> map = new MyMapNode<int, string>();
+            map.Add(0, "to");
+            map.Add(1, "be");
+            map.Add(2, "or");
+            map.Add(3, "not");
+            map.Add(4, "to");
+            map.Add(5, "be");
+            map.GetFreq();
+
+        }
+    }
+}
