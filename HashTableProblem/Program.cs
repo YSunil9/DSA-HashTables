445d2ae74d046a8fc9b98687e61d65ea29b4da16
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-22 20:41:18 +0530

    Remove Word From Paragraph

--- HashTableProblem/Program.cs
+++ HashTableProblem/Program.cs
@@ -4,19 +4,36 @@
     {
         static void Main(string[] args)
         {
-            MyMapNode<int, string> map = new MyMapNode<int, string>(1);
-            map.Add(0, "“Paranoids are not paranoid because they are" +
-                       " paranoid but because they keep putting themselves " +
-                       "deliberately into paranoid avoidable situations");
-            string map0 = map.Get(0);
-            Console.WriteLine(map0);
-            Console.WriteLine("\nFrequency of Words\n");
-            string[] arr = new string[map.size];
+            MyMapNode<int, string> map = new MyMapNode<int, string>(19);
+            map.Add(0, "Paranoids");
+            map.Add(1, "are");
+            map.Add(2, "not");
+            map.Add(3, "paranoid");
+            map.Add(4, "because");
+            map.Add(5, "they");
+            map.Add(6, "are");
+            map.Add(7, "paranoid");
+            map.Add(8, "but");
+            map.Add(9, "because");
+            map.Add(10, "they");
+            map.Add(11, "keep");
+            map.Add(12, "putting");
+            map.Add(13, "themselves");
+            map.Add(14, "deliberately");
+            map.Add(15, "into");
+            map.Add(16, "paranoid");
+            map.Add(17, "avoidable");
+            map.Add(18, "situations");
             for (int i = 0; i < map.size; i++)
             {
-                arr[i] = map.Get(0);
+                Console.WriteLine($"{i} index value : {map.Get(i)}");
             }
-            map.FreqOfWords(arr, arr.Length);
+            Console.WriteLine("\nRemoving a Word from Paragraph");
+            map.Remove(10);
+            for (int i = 0; i < map.size; i++)
+            {
+                Console.WriteLine($"{i} index value : {map.Get(i)}");
+            }        
         }
     }
 }
